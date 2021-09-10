7e8b981ceaf9e3be20ba5ceb67aace274f380fda
Blattttt <77572968+Blattttt@users.noreply.github.com>
2021-09-10 15:28:41 +0300

    Update MainActivityLife.java

--- MainActivityLife.java
+++ MainActivityLife.java
@@ -12,30 +12,33 @@ public class MainActivity extends Activity {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.main);
 	Log.d(TAG, "onCreate");
-	setContentView(R.layout.activity_main);
 	Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void onRestoreInstanceState(Bundle savedInstanceState) {
 	super.onRestoreInstanceState(savedInstanceState);
+	setContentView(R.layout.main);
 	if (savedInstanceState != null && savedInstanceState.containsKey("count"))
 	{
 		count = savedInstanceState.getInt("count");
 	}
 	Log.d(TAG, "onRestoreInstanceState");
+	Toast.makeText(this, "onRestoreInstanceState", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void onRestart(){
 	super.onRestart();
 	Log.d(TAG, "onRestart");
+	Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void onStart(){
 	super.onStart();
 	Log.d(TAG, "onStart");
+	Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
 	resetUI();
 	}
 
@@ -43,6 +46,7 @@ public class MainActivity extends Activity {
 	public void onResume(){
 	super.onResume();
 	Log.d(TAG, "onResume");
+	Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
 
 	}
 
@@ -51,23 +55,27 @@ public class MainActivity extends Activity {
 	super.onSaveInstanceState(savedInstanceState);
 	outState.putInt("count", count);
 	Log.d(TAG, "onSaveInstanceState");
+	Toast.makeText(this, "onSaveInstanceState", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void onPause(){
 	super.onPause();
 	Log.d(TAG, "onPause");
+	Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void onStop(){
 	super.onStop();
 	Log.d(TAG, "onStop");
+	Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void onDestroy(){
 	super.onDestroy();
 	Log.d(TAG, "onDestroy");
+	Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
 	}
 }
